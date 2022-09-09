2e73659a5ffd3e1c1f2fe3fbac21a1ea1704c34e
Andrew Schastlivcev <andrew.schastlivcev@sparkybit.com>
2022-09-09 16:33:18 +0300

    fixed untracked files

--- src/cms/custom-cms.js
+++
@@ -1,5 +0,0 @@
-import idWidget from 'netlify-cms-widget-simple-uuid';
-
-if (typeof window !== 'undefined') {
-  window.NetlifyCmsApp.registerWidget('id', idWidget.IdControl, idWidget.IdPreview);
-}
