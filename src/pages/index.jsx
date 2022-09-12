26744faf0b79fe972dcb85d3cd7645dc16c5fbe6
Andrew Schastlivcev <dj_winston@outlook.com>
2022-09-13 00:25:23 +0300

    Add plugin

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,16 @@
-import * as React from 'react'
-import { StaticImage/* , getImage, GatsbyImage */ } from 'gatsby-plugin-image'
-// import { graphql } from 'gatsby'
-// import { MDXRenderer } from 'gatsby-plugin-mdx'
-// import { MDXProvider } from "@mdx-js/react"
+import * as React from "react"
+import { StaticImage, getImage, GatsbyImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { MDXProvider } from "@mdx-js/react"
 
 // styles
 const pageStyles = {
-  color: '#232129',
+  color: "#232129",
   padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
   img: {
-    width: '50%',
+    width: "50%",
   },
 }
 const headingStyles = {
@@ -19,147 +19,53 @@ const headingStyles = {
   maxWidth: 320,
 }
 const headingAccentStyles = {
-  color: '#663399',
+  color: "#663399",
 }
 const paragraphStyles = {
   marginBottom: 48,
 }
 const codeStyles = {
-  color: '#8A6534',
+  color: "#8A6534",
   padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
   borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: '#8954A8',
-  fontWeight: 'bold',
-  fontSize: 16,
-  verticalAlign: '5%',
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: 'none',
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: '#232129',
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: 'Documentation',
-  url: 'https://www.gatsbyjs.com/docs/',
-  color: '#8954A8',
-}
-
-const badgeStyle = {
-  color: '#fff',
-  backgroundColor: '#088413',
-  border: '1px solid #088413',
-  fontSize: 11,
-  fontWeight: 'bold',
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: '4px 6px',
-  display: 'inline-block',
-  position: 'relative',
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
 }
 
 const hr = {
   border: 0,
-  clear: 'both',
-  width: '96%',
-  backgroundColor: 'red',
-  height: '1px',
+  clear: "both",
+  width: "96%",
+  backgroundColor: "red",
+  height: "1px",
 }
-
-// data
-const links = [
-  {
-    text: 'Tutorial',
-    url: 'https://www.gatsbyjs.com/docs/tutorial/',
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: '#E95800',
-  },
-  {
-    text: 'How to Guides',
-    url: 'https://www.gatsbyjs.com/docs/how-to/',
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: '#1099A8',
-  },
-  {
-    text: 'Reference Guides',
-    url: 'https://www.gatsbyjs.com/docs/reference/',
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: '#BC027F',
-  },
-  {
-    text: 'Conceptual Guides',
-    url: 'https://www.gatsbyjs.com/docs/conceptual/',
-    description:
-      'Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.',
-    color: '#0D96F2',
-  },
-  {
-    text: 'Plugin Library',
-    url: 'https://www.gatsbyjs.com/plugins',
-    description:
-      'Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.',
-    color: '#8EB814',
-  },
-  {
-    text: 'Build and Host',
-    url: 'https://www.gatsbyjs.com/cloud',
-    badge: true,
-    description:
-      'Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!',
-    color: '#663399',
-  },
-]
 
 // markup
 const IndexPage = ({ data }) => {
-  // console.log(`TCL>>> ~ data`, data)
-  // const images = data.allMdx.edges[0].node.frontmatter
-  // const image1 = getImage(images.featuredImage)
-  // const image2 = getImage(images.mainPostImage)
-  // const pageBody = data.allMdx.edges[0].node.body
+  console.log(`TCL>>> ~ data`, data)
+  const images = data.allMdx.nodes[0].frontmatter
+  const image1 = getImage(images.featuredImage)
+  const image2 = getImage(images.mainPostImage)
+  const image3 = getImage(images.postImage)
+  const pageBody = data.allMdx.nodes[0].body
   // console.log(`TCL>>> ~ pageBody`, pageBody)
   // console.log(`TCL>>> ~ images`, images)
-  // const myParagraph = props => (
-  //   <p style={{backgroundColor: 'yellow'}} {...props}/>
-  // )
-  // const myImg = props => {
-  //     return (
-  //   <img style={{width: '100%'}} {...props}/>
-  // )}
-  // const components = {
-  //   p: myParagraph,
-  //   img: myImg
-  // }
+  const myParagraph = (props) => <p style={{ backgroundColor: "yellow" }} {...props} />
+  const myImg = (props) => {
+    console.log("img props", props.src)
+    // const src = props.src
+    // const pathSrc = "../../static/img/mahsa-gholami-4x8liUBelKo-unsplash.jpg"
+    //<StaticImage /* src={src} alt={props.alt} */ {...props} />
+    return (
+      <>
+        <img style={{ width: "100%" }} {...props} alt="random props" />
+      </>
+    )
+  }
+  const components = {
+    p: myParagraph,
+    img: myImg,
+  }
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
@@ -172,44 +78,16 @@ const IndexPage = ({ data }) => {
         </span>
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page update in real-time.{' '}
+        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page update in real-time.{" "}
         <span role="img" aria-label="Sunglasses smiley emoji">
           😎
         </span>
       </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map((link) => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
       <img
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
         <StaticImage width={1000} quality="100" src="../images/moncell-allen-NJP21VtDrZQ-unsplash.jpg" alt="sexy" />
         <div>
           <StaticImage width={500} quality="100" src="../images/roman-khripkov-HxIJP-yFLyQ-unsplash.jpg" alt="sexy" />
@@ -218,19 +96,20 @@ const IndexPage = ({ data }) => {
         </div>
       </div>
       <hr style={hr} />
-      <div style={{ textAlign: 'center' }}>DYNAMIC IMAGES</div>
+      <div style={{ textAlign: "center" }}>DYNAMIC IMAGES</div>
       <hr style={hr} />
-      {/* <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
         <GatsbyImage image={image1} alt="page image1" />
         <GatsbyImage image={image2} alt="page image2" />
-      </div> */}
+        <GatsbyImage image={image3} alt="page image3" />
+      </div>
       <hr style={hr} />
-      <div style={{ textAlign: 'center' }}>PAGE MD</div>
+      <div style={{ textAlign: "center" }}>PAGE MDX CONTENT</div>
       <hr style={hr} />
-      <div style={{ width: '100%' }}>
-        {/* <MDXProvider components={components}>
+      <div style={{ width: "100%" }}>
+        <MDXProvider components={components}>
           <MDXRenderer data={data}>{pageBody}</MDXRenderer>
-        </MDXProvider> */}
+        </MDXProvider>
       </div>
     </main>
   )
@@ -238,42 +117,55 @@ const IndexPage = ({ data }) => {
 
 export default IndexPage
 
-// export const query = graphql`
-//   query HomePageQuery {
-//     allMdx(filter: { frontmatter: { templateKey: { eq: "mdx-content" } } }) {
-//       edges {
-//         node {
-//           frontmatter {
-//             featuredImage {
-//               childImageSharp {
-//                 gatsbyImageData(
-//                   layout: CONSTRAINED
-//                   webpOptions: { quality: 95 }
-//                   quality: 95
-//                   breakpoints: [360, 768, 1024]
-//                 )
-//               }
-//             }
-//             mainPostImage {
-//               childImageSharp {
-//                 gatsbyImageData(
-//                   layout: CONSTRAINED
-//                   webpOptions: { quality: 95 }
-//                   quality: 95
-//                   breakpoints: [360, 768, 1024]
-//                 )
-//               }
-//               internal {
-//                 type
-//               }
-//               relativePath
-//               publicURL
-//             }
-//           }
-//           body
-//           fileAbsolutePath
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query HomePageQuery {
+    allMdx(filter: { frontmatter: { templateKey: { eq: "mdx-content" } } }) {
+      nodes {
+        frontmatter {
+          featuredPost
+          featuredImage {
+            publicURL
+            absolutePath
+            childImageSharp {
+              gatsbyImageData(
+                layout: CONSTRAINED
+                webpOptions: { quality: 95 }
+                quality: 95
+                breakpoints: [360, 768, 1024]
+              )
+            }
+            relativePath
+          }
+          mainPostImage {
+            publicURL
+            absolutePath
+            relativePath
+            childImageSharp {
+              gatsbyImageData(
+                layout: CONSTRAINED
+                webpOptions: { quality: 95 }
+                quality: 95
+                breakpoints: [360, 768, 1024]
+              )
+            }
+          }
+          postImage {
+            childImageSharp {
+              gatsbyImageData(
+                layout: CONSTRAINED
+                webpOptions: { quality: 95 }
+                quality: 95
+                breakpoints: [360, 768, 1024]
+              )
+            }
+          }
+        }
+        fileAbsolutePath
+        internal {
+          type
+        }
+        body
+      }
+    }
+  }
+`
